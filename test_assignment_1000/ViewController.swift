f994c4684895ef2ac2ed85d140afa8de341ba29a
Farkhat Samarkanov <farhatsamarkanov@gmail.com>
2020-01-24 22:34:54 +0300

    Initial Commit

---
+++ test_assignment_1000/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_assignment_1000
+//
+//  Created by Admin on 24/01/2020.
+//  Copyright © 2020 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
